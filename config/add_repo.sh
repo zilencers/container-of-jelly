2902610f8166f6ccb17afd353be630139f15511c
zilencer <zilencer@protonmail.com>
2023-11-10 23:43:07 -0600

    Bug fixes and updates

--- config/add_repo.sh
+++ config/add_repo.sh
@@ -4,7 +4,7 @@ export VERSION_OS="$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )"
 export VERSION_CODENAME="$( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )"
 export DPKG_ARCHITECTURE="$( dpkg --print-architecture )"
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
+cat <<EOF | tee /etc/apt/sources.list.d/jellyfin.sources
 Types: deb
 URIs: https://repo.jellyfin.org/${VERSION_OS}
 Suites: ${VERSION_CODENAME}
--- install.sh
+++ install.sh
@@ -7,10 +7,22 @@ podman build -t jellyfin -f config/Dockerfile
 podman run -d \
 -v /etc/opt/jellyfin/config:/config \
 -v /var/cache/jellyfin/cache:/cache \
--v /srv/jellyfin/media/movies:/movies \
--v /srv/jellyfin/media/shows:/shows \
--v /srv/jellyfin/media/music:/music \
--v /srv/jellyfin/media/photos:/photos \
+-v /srv/jellyfin/media/movies:/srv/movies \
+-v /srv/jellyfin/media/shows:/srv/shows \
+-v /srv/jellyfin/media/music:/srv/music \
+-v /srv/jellyfin/media/photos:/srv/photos \
+-v /srv/jellyfin/media/dvr:/srv/dvr \
+-v /srv/jellyfin/family/movies:/srv/family/movies \
+-v /srv/jellyfin/family/music:/srv/family/music \
+-v /srv/jellyfin/family/shows:/srv/family/shows \
+-v /srv/jellyfin/family/photos:/srv/family/photos \
+-v /srv/jellyfin/users/mom:/srv/users/mom \
+-v /srv/jellyfin/users/mom/music:/srv/users/mom/music \
+-v /srv/jellyfin/users/brandon:/srv/users/brandon \
+-v /srv/jellyfin/users/brandon/music:/srv/users/brandon/music \
+-v /srv/jellyfin/users/brandon/photos:/srv/users/brandon/photos \
+-v /srv/jellyfin/users/brandon/movies:/srv/users/brandon/movies \
+-v /srv/jellyfin/users/brandon/shows:/srv/users/brandon/shows \
 --name jellyfin \
 --net=host \
 --restart always \
