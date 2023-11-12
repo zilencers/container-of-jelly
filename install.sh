a394273c0cae5169983243812de0eb2080f32512
zilencer <zilencer@protonmail.com>
2023-11-12 00:10:28 -0600

    various updates

--- install.sh
+++ install.sh
@@ -12,18 +12,13 @@ podman run -d \
 -v /srv/jellyfin/media/music:/srv/music \
 -v /srv/jellyfin/media/photos:/srv/photos \
 -v /srv/jellyfin/media/dvr:/srv/dvr \
--v /srv/jellyfin/family/movies:/srv/family/movies \
--v /srv/jellyfin/family/music:/srv/family/music \
--v /srv/jellyfin/family/shows:/srv/family/shows \
--v /srv/jellyfin/family/photos:/srv/family/photos \
--v /srv/jellyfin/users/mom:/srv/users/mom \
 -v /srv/jellyfin/users/mom/music:/srv/users/mom/music \
--v /srv/jellyfin/users/brandon:/srv/users/brandon \
 -v /srv/jellyfin/users/brandon/music:/srv/users/brandon/music \
 -v /srv/jellyfin/users/brandon/photos:/srv/users/brandon/photos \
 -v /srv/jellyfin/users/brandon/movies:/srv/users/brandon/movies \
 -v /srv/jellyfin/users/brandon/shows:/srv/users/brandon/shows \
+--label "io.containers.autoupdate=registry" \
 --name jellyfin \
 --net=host \
---restart always \
 localhost/jellyfin:latest
+
