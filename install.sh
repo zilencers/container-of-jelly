dbcae41e0fb8cf33d45001bceee503f11fd8b12e
zilencer <zilencer@protonmail.com>
2023-11-14 01:38:51 -0600

    Added setsebool for containers, modified created directories, add fstab nfs mounts

--- install.sh
+++ install.sh
@@ -5,20 +5,19 @@ podman build -t jellyfin -f config/Dockerfile
 
 # Run the container
 podman run -d \
--v /etc/opt/jellyfin/config:/config \
--v /var/cache/jellyfin/cache:/cache \
--v /srv/jellyfin/media/movies:/srv/movies \
--v /srv/jellyfin/media/shows:/srv/shows \
--v /srv/jellyfin/media/music:/srv/music \
--v /srv/jellyfin/media/photos:/srv/photos \
--v /srv/jellyfin/media/dvr:/srv/dvr \
--v /srv/jellyfin/users/mom/music:/srv/users/mom/music \
--v /srv/jellyfin/users/brandon/music:/srv/users/brandon/music \
--v /srv/jellyfin/users/brandon/photos:/srv/users/brandon/photos \
--v /srv/jellyfin/users/brandon/movies:/srv/users/brandon/movies \
--v /srv/jellyfin/users/brandon/shows:/srv/users/brandon/shows \
---label "io.containers.autoupdate=registry" \
---name jellyfin \
---net=host \
-localhost/jellyfin:latest
-
+  --name jellyfin \
+  --label "io.containers.autoupdate=registry" \
+  --net=host \
+  --user $(id -u):$(id -g) \
+  --userns keep-id \
+  -v /etc/opt/jellyfin/config:/config:Z \
+  -v /var/cache/jellyfin/cache:/cache:Z \
+  -v /mnt/movies:/mnt/movies:Z \
+  -v /mnt/shows:/mnt/show:Z \
+  -v /mnt/music:/mnt/music:Z \
+  -v /mnt/photos:/mnt/photos:Z \
+  -v /srv/local/media/movies:/movies:Z \ 
+  -v /srv/local/media/shows:/shows:Z \
+  -v /srv/local/media/music:/music:Z \
+  -v /srv/local/media/photos:/photos:Z \
+  localhost/jellyfin:latest
