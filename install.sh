de3bfdcbe0e8afac382905b1bc2379af61b3ebe1
zilencer <zilencer@protonmail.com>
2023-11-18 14:21:29 -0600

    Removed :Z option from volumes

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ podman build -t jellyfin -f config/Dockerfile
 
 # Run the container
 podman run -d \
-  --name jellyfin \
-  --label "io.containers.autoupdate=registry" \
-  --net=host \
-  --user $(id -u):$(id -g) \
-  --userns keep-id \
-  -v /etc/opt/jellyfin/config:/config:Z \
-  -v /var/cache/jellyfin/cache:/cache:Z \
-  -v /srv/share/movies:/mnt/movies:Z \
-  -v /srv/share/shows:/mnt/shows:Z \
-  -v /srv/share/music:/mnt/music:Z \
-  -v /srv/photos:/mnt/photos:Z \
-  -v /srv/local/media/movies:/movies:Z \ 
-  -v /srv/local/media/shows:/shows:Z \
-  -v /srv/local/media/music:/music:Z \
-  -v /srv/local/media/photos:/photos:Z \
-  localhost/jellyfin:latest
+ --name jellyfin \
+ --label "io.containers.autoupdate=registry" \
+ --net=host \
+ --user $(id -u):$(id -g) \
+ --userns keep-id \
+ --volume /etc/opt/jellyfin/config:/config \
+ --volume /var/cache/jellyfin/cache:/cache \
+ --volume /srv/share/movies:/mnt/movies \
+ --volume /srv/share/shows:/mnt/shows \
+ --volume /srv/share/music:/mnt/music \
+ --volume /srv/share/photos:/mnt/photos \
+ --volume /srv/local/media/movies:/movies \
+ --volume /srv/local/media/shows:/shows \
+ --volume /srv/local/media/music:/music \
+ --volume /srv/local/media/photos:/photos \
+ localhost/jellyfin:latest
