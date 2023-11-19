bd5bf6543c681be0180c45cb5b7faa7d41cde620
zilencer <zilencer@protonmail.com>
2023-11-19 00:44:54 -0600

    various bug fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enable linger
-loginctl enable-linger $USER
+#loginctl enable-linger $USER
 
 # Build the podman image
 podman build -t jellyfin -f config/Dockerfile
@@ -9,18 +9,15 @@ podman build -t jellyfin -f config/Dockerfile
 # Run the container
 podman run -d \
  --name jellyfin \
- --label "io.containers.autoupdate=registry" \
  --net=host \
- --user $(id -u):$(id -g) \
- --userns keep-id \
- --volume /etc/opt/jellyfin/config:/config \
- --volume /var/cache/jellyfin/cache:/cache \
+ --volume /etc/opt/jellyfin/config:/config:Z \
+ --volume /var/cache/jellyfin/cache:/cache:Z \
  --volume /srv/share/movies:/mnt/movies \
  --volume /srv/share/shows:/mnt/shows \
  --volume /srv/share/music:/mnt/music \
  --volume /srv/share/photos:/mnt/photos \
- --volume /srv/local/media/movies:/movies \
- --volume /srv/local/media/shows:/shows \
- --volume /srv/local/media/music:/music \
- --volume /srv/local/media/photos:/photos \
+ --volume /srv/local/media/movies:/movies:Z \
+ --volume /srv/local/media/shows:/shows:Z \
+ --volume /srv/local/media/music:/music:Z \
+ --volume /srv/local/media/photos:/photos:Z \
  localhost/jellyfin:latest
